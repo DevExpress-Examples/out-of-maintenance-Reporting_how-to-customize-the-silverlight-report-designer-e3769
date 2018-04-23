ce842c756b466eca16da617b3575165b5a4ab3c1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication_ReportDesigner_CustomButton/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace SilverlightApplication_ReportDesigner_CustomButton {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            DataContext = new MainPageViewModel();
+        }
+    }
+}
